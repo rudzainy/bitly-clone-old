3176004170f25495c7604cb85c3dfa798ed825b9
rudzainy <rudzainy@gmail.com>
2016-09-02 09:47:25 +0800

    Beautify app

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,19 +1,29 @@
 get '/' do
+  @urls = Url.all.order("click_count DESC")
   erb :"static/index"
 end
 
 post '/urls' do
-  url = Url.new(long_url: params[:long_url])
-  url.shorten
-  if url.save
-    redirect to '/'
-  else
-    @flash = "Error"
+  url = Url.find_by_long_url(params[:long_url])
+  if url
+    @flash = "#{url.long_url} has already been added before."
+    @urls = Url.all.order("click_count DESC")
     erb :'static/index'
+  else
+    url = Url.new(long_url: params[:long_url])
+    url.shorten
+    if url.save
+      redirect to '/'
+    else
+      @flash = "Invalid URL. Did you include 'http://' or 'https://'?"
+      @urls = Url.all.order("click_count DESC")
+      erb :'static/index'
+    end
   end
 end
 
 get '/:short_url' do
   url = Url.find_by_short_url(params[:short_url])
+  url.add_click_count
   redirect to url.long_url
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,10 +1,13 @@
 class Url < ActiveRecord::Base
 
-  validates :short_url, uniqueness: true
-  validates :long_url, uniqueness: true
+  validates :short_url, uniqueness: true, presence: true
+  validates :long_url, uniqueness: true, presence: true, format: { with: URI.regexp }, if: Proc.new { |a| a.long_url.present? }
 
 	def shorten
     self.short_url = ([*('A'..'Z'),*('0'..'9')]-%w(0 1 I O)).sample(8).join
-    self.shorten unless self.save
+  end
+
+  def add_click_count
+    self.update(click_count: self.click_count + 1)
   end
 end
